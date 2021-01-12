560512800561ca33e380a4b94e07c1840ff1ec11
phebian <phebian.chukwurah@chlela.com>
2021-01-12 19:48:25 +0100

    updates to stories

--- src/stories/Select.stories.js
+++ src/stories/Select.stories.js
@@ -1,23 +1,3 @@
-// import React from "react";
-// import { Select } from "components";
-// import { action } from "@storybook/addon-actions";
-// export default {
-//   title: "Example/Select",
-//   component: Select,
-// };
-
-// const Template = (args) => <Select {...args} />;
-
-// export const Default = Template.bind({});
-// Default.args = {
-//   label: "Country",
-//   name: "country",
-//   onChange: action("select change"),
-//   defaultValue: "",
-//   placeholder: "Select Country",
-//   options: ["Nigeria", "Ghana", "Kenya"],
-// };
-
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Select } from "components";
@@ -25,5 +5,5 @@ import { Select } from "components";
 const options = ["Nigeria", "Ghana", "Kenya"];
 
 storiesOf("Select", module)
-  .add("Simple usage", () => <Select label="Country" options={options} />)
+  .add("Default Select", () => <Select label="Country" defaultValue="" options={options} />)
 
